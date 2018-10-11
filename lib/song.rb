a5134dd22db0cbc7af79ad15371d0033af1692b0
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 10:41:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,14 +49,14 @@ class Song
     song
   end
   
-  # def self.create_from_filename(name)
-  # #class method should not only parse the filename correctly but should also save the song
-  #   song = self.new
-  #   song.name = (name.split(" - ")[1].chomp(".mp3"))
-  #   song.artist_name = (name.split(" - ")[0])
-  #   @@all << song
-  #   song
-  # end
+  def self.create_from_filename(name)
+  #class method should not only parse the filename correctly but should also save the song
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
   
   def self.destroy_all()
     #reset the state of the @@all class variable to an empty array thereby deleting all previous song instances.
