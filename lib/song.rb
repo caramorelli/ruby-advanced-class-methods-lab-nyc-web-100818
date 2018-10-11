d5a32d726a14826cfdbbba68338deab8a5c0dccc
Cara Morelli <caramorelli7@gmail.com>
2018-10-11 10:32:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,27 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def create
+    @song = song
+  end 
+  
+  def create_by_name(song_name)
+    @@all << song_name
+  end 
+  
+  def find_by_name(song_name)
+    @@all.include?(song_name)
+  end 
+  
+  def find_or_create_by_name(song_name)
+    unless @@all.include?(song_name) 
+      @@all << song_name
+    end 
+  end 
+  
+  def alphabetical 
+    
+  end 
 
 end
